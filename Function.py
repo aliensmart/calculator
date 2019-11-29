925a81e4f062774cb9ac474072077366ff7e5198
aliensmart <kaoua17@gmail.com>
2019-11-28 23:12:35 -0500

    function with event in tkinter

--- Function.py
+++ Function.py
@@ -9,7 +9,19 @@ def say_hi():
     
     Label(window, text = "Hi").pack()
 
+def left_click(event):
+    Label(window, text = "Left Click").pack()
+
+def right_click(event):
+    Label(window, text = "Right CLick").pack()
+
+def middle_click(mousemove):
+    Label(window, text = "Middle Click").pack()
 
 Button(window, text = "Click me", command = say_hi).pack()
+
+window.bind("<Button-1>", left_click)
+window.bind("mousemove",middle_click)
+window.bind("<Button-3>", right_click)
 
 window.mainloop()
